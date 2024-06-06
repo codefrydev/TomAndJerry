de8e282582af329df375484537d1203439280a1d
Prashant Priyadarshi <48068632+PrashantUnity@users.noreply.github.com>
2024-06-06 15:49:27 +0530

    Please For Give Me  if There is any bug

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using TomAndJerry;
+using TomAndJerry.DataBase;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+ 
+builder.Services.AddSingleton<Data>();
 await builder.Build().RunAsync();
