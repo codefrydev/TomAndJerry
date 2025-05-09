0bbb5cb93110b961c794949078d3c4abbfba1cf8
PrashantUnity <priyadarshisadiku@gmail.com>
2025-05-09 22:51:31 +0530

    Commit Update

--- DataBase/Data.cs
+++ DataBase/Data.cs
@@ -1,115 +1,104 @@
 ﻿using TomAndJerry.Model;
 
-namespace TomAndJerry.DataBase;
-
-public class Data
+namespace TomAndJerry.DataBase
 {
-    public event Action? OnChange;
-
-    private void NotifyDataChanged()
+    public class Data
     {
-        OnChange?.Invoke();
-    }
+        public event Action OnChange;
+        private void NotifyDataChanged() => OnChange?.Invoke();
 
-    private List<Video> _data = [];
-    private bool _isDataFetched;
-
-    public List<Video> VideosData
-    {
-        get => _data;
-        private set
+        private List<Video> _data = [];
+        private bool _isDataFetched;
+        public List<Video> VideosData
         {
-            _data = value;
-            NotifyDataChanged();
-        }
-    }
-
-    private bool _isDataFetced = false;
-
-    private List<Video> _filteredData = [];
-
-    public List<Video> FilteredData
-    {
-        get => _filteredData;
-        set
-        {
-            _filteredData = value;
-            NotifyDataChanged();
-        }
-    }
-
-
-    #region permanentUri
-
-    private static readonly Random Random = new();
-
-    private const string EpisodeNameUri =
-        "https://raw.githubusercontent.com/TomJerry1940/Database/main/EpisodeNameList.txt";
-
-    private const string DriveVideoUri =
-        "https://raw.githubusercontent.com/TomJerry1940/Database/main/GoogleDriveVideoUri.txt";
-
-    private const string ThumbnailPath =
-        "https://raw.githubusercontent.com/TomJerry1940/Database/main/ThumbnailPath.txt";
-
-    #endregion
-
-    private static List<T> SuffeledArray<T>(List<T> array)
-    {
-        for (var i = 0; i < array.Count; i++)
-        {
-            var randIndex = Random.Next(i, array.Count);
-            (array[randIndex], array[i]) = (array[i], array[randIndex]);
+            get => _data;
+            private set
+            {
+                _data = value;
+                NotifyDataChanged();
+            }
         }
 
-        return array;
-    }
+        public bool IsDataFetced = false;
 
-    public Video GetVideo(string id)
-    {
-        return VideosData.FirstOrDefault(x => x.Id == id) ?? VideosData[0];
-    }
+        private List<Video> _filteredData = [];
 
-    public List<Video> GetRandomVideo()
-    {
-        return SuffeledArray(VideosData);
-    }
+        public List<Video> FilteredData
+        {
+            get => _filteredData;
+            set
+            {
+                _filteredData = value;
+                NotifyDataChanged();
+            }
+        }
 
-    public async Task InitializeAsync()
-    {
-        if (_isDataFetched) return;
-        VideosData = await FetchDataAsync();
-        _isDataFetched = true;
-    }
 
-    private async Task<List<Video>> FetchDataAsync()
-    {
-        if (_isDataFetced) return VideosData;
-        using var client = new HttpClient();
-        var videoListString = await client.GetStringAsync(DriveVideoUri);
-        var episodeNameString = await client.GetStringAsync(EpisodeNameUri);
-        var thumbnailString = await client.GetStringAsync(ThumbnailPath);
+        #region permanentUri
 
-        var videoUriList = videoListString
-            .Split('\n').Where(x => x.Length > 0).Select(x => x.Trim()).ToList();
-        var episodeList = episodeNameString
-            .Split('\n').Where(x => x.Length > 0).Select(x => x.Trim()).ToList();
-        var thumbnailList = thumbnailString
-            .Split('\n').Where(x => x.Length > 0).Select(x => x.Trim()).ToList();
-        VideosData = [];
-        for (var i = 0; i < Math.Min(videoUriList.Count, episodeList.Count); i++)
-            VideosData.Add(
-                new Video
-                {
-                    Id = $"{i+1}",
-                    Thumbnail = thumbnailList[i],
-                    Description = episodeList[i],
-                    VideoId = videoUriList[i],
-                    CommentName = episodeList[i],
-                    VideoUrl = videoUriList[i]
-                }
-            );
-        _isDataFetced = true;
-        return VideosData;
+        private static Random _random = new Random();
+        private const string EpisodeNameUri = "https://raw.githubusercontent.com/TomJerry1940/Database/main/EpisodeNameList.txt";
+        private const string DriveVideoUri = "https://raw.githubusercontent.com/TomJerry1940/Database/main/GoogleDriveVideoUri.txt";
+        private const string ThumbnailPath = "https://raw.githubusercontent.com/TomJerry1940/Database/main/ThumbnailPath.txt";
+
+        #endregion
+        static List<T> SuffeledArray<T>(List<T> array)
+        {
+
+            for (var i = 0; i < array.Count; i++)
+            {
+                var randIndex = _random.Next(i, array.Count);
+                (array[randIndex], array[i]) = (array[i], array[randIndex]);
+            }
+            return array;
+        }
+        public Video GetVideo(string id)
+        {
+            return VideosData.FirstOrDefault(x => x.Id == id) ?? VideosData[0];
+        }
+        public List<Video> GetRandomVideo()
+        {
+            return SuffeledArray(VideosData);
+        }
+
+        public async Task InitializeAsync()
+        {
+            if (_isDataFetched) return;
+            VideosData = await FetchDataAsync();
+            _isDataFetched = true;
+        }
+
+        private async Task<List<Video>> FetchDataAsync()
+        {
+            if (IsDataFetced) return VideosData;
+            using var client = new HttpClient();
+            var videoListString = await client.GetStringAsync(DriveVideoUri);
+            var episodeNameString = await client.GetStringAsync(EpisodeNameUri);
+            var thumbnailString = await client.GetStringAsync(ThumbnailPath);
+
+            var videoUriList = videoListString
+                .Split('\n').Where(x => x.Length > 0).Select(x => x.Trim()).ToList();
+            var episodeList = episodeNameString
+                .Split('\n').Where(x => x.Length > 0).Select(x => x.Trim()).ToList();
+            var thumbnailList = thumbnailString
+                .Split('\n').Where(x => x.Length > 0).Select(x => x.Trim()).ToList();
+            VideosData = [];
+            for (var i = 0; i < Math.Min(videoUriList.Count, episodeList.Count); i++)
+            {
+                VideosData.Add(
+                    new Video()
+                    {
+                        Id = $"{i+1}",
+                        Thumbnail = thumbnailList[i],
+                        Description = episodeList[i],
+                        VideoId = videoUriList[i],
+                        CommentName = episodeList[i],
+                        VideoUrl = videoUriList[i]
+                    }
+               );
+            }
+            IsDataFetced = true;
+            return VideosData;
+        }
     }
 }
--- Model/Video.cs
+++ Model/Video.cs
@@ -1,14 +1,15 @@
-﻿namespace TomAndJerry.Model;
-
-public class Video
+﻿namespace TomAndJerry.Model
 {
-    public string Id { get; set; }
-    public string Thumbnail { get; set; } = string.Empty;
-    public string CommentName { get; set; } = string.Empty;
+    public class Video
+    {
+        public string Id { get; set; }
+        public string Thumbnail { get; set; } = string.Empty;
+        public string CommentName { get; set; } = string.Empty;
 
-    public string VideoId { get; set; } = string.Empty;
+        public string VideoId { get; set; } = string.Empty;
 
-    public string Description { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
 
-    public string VideoUrl { get; set; } = string.Empty;
+        public string VideoUrl { get; set; } = string.Empty;
+    }
 }
