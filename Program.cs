556b754f1108c35476f16a111387b8c70a9361b0
PrashantUnity <priyadarshisadiku@gmail.com>
2025-09-07 21:55:37 +0530

    Initial working

--- Program.cs
+++ Program.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
 using TomAndJerry;
 using TomAndJerry.DataBase;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
